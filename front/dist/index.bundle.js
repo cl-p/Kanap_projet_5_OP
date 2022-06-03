bfa76f87fad478a5ca8b75b6a026a9fca04237a7
Clémentine Pardieu <cl.pardieu@gmail.com>
2022-06-03 17:35:31 +0200

    corrections finales

--- front/dist/index.bundle.js
+++ front/dist/index.bundle.js
@@ -1,1 +1,32 @@
-fetch("http://localhost:3000/api/products").then((e=>{e.json().then((e=>{e.forEach((e=>{!function(e){let t=document.getElementById("items"),c=document.createElement("a");c.href="./product.html?id="+e._id;let n=document.createElement("article"),a=document.createElement("img");a.src=e.imageUrl,a.alt=e.altTxt;let l=document.createElement("h3");l.className="productName",l.innerText=e.name;let m=document.createElement("p");m.className="productDescription",m.innerText=e.description,c.appendChild(n),n.append(a,l,m),t.appendChild(c)}(e)}))}))})).catch((e=>{console.log(e)}));
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./js/script.js":
+/*!**********************!*\
+  !*** ./js/script.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("\r\nfetch(\"http://localhost:3000/api/products\").then(response => {\r\n   response.json().then(value => {\r\n        value.forEach(canape => {\r\n            creationCard(canape);\r\n        });\r\n\r\n   })\r\n}).catch(reason => {\r\n    console.log(reason)\r\n})\r\n\r\n// cette fonction va créer tout l'HTML correspondant pour l'accueil\r\nfunction creationCard(c){\r\n    let parent = document.getElementById(\"items\");\r\n    // créer a \r\n    let link = document.createElement(\"a\");\r\n    // ajouter attribut href à a \r\n    link.href = \"./product.html?id=\" + c._id;\r\n    // créer article comme enfant de a \r\n    let article = document.createElement(\"article\");\r\n    // créer img enfant de article\r\n    let img = document.createElement(\"img\");\r\n    // Ajouter src à img\r\n    img.src = c.imageUrl;\r\n    // ajouter alt à img\r\n    img.alt = c.altTxt\r\n    // créer h3 \r\n    let h3 = document.createElement(\"h3\");\r\n    // ajouter class à h3\r\n    h3.className = \"productName\";\r\n    // ajouter texte à h3 \r\n    h3.innerText = c.name;\r\n    //créer p \r\n    let p = document.createElement(\"p\");\r\n    //ajouter class à p\r\n    p.className = \"productDescription\";\r\n    // ajouter texte à p\r\n    p.innerText = c.description\r\n\r\n    \r\n    link.appendChild(article);\r\n    article.append(img, h3, p);\r\n    parent.appendChild(link);\r\n    \r\n}\n\n//# sourceURL=webpack://kanap-front-end/./js/script.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./js/script.js"]();
+/******/ 	
+/******/ })()
+;
--- front/js/panier.js
+++ front/js/panier.js
@@ -1,6 +1,31 @@
 
 export class Panier {
-    id
-    quantite
-    couleur
+    ajoutPanier(id, quantite, couleur){
+        let produit = {
+            idProduit: id,
+            quantiteProduit: quantite,
+            couleurProduit: couleur
+        }
+    
+       // Récupération d'articles stockés dans localstorage 
+       // renvoi le tableau d'objet (propriétés) en format string
+        let recuperationPanier = localStorage.getItem("panier");
+        // transformation du format string en tableau
+        let articles = JSON.parse(recuperationPanier)
+        // ajout objet à la fin du tableau
+        articles.push(produit)
+
+        // ajout  nouveau tableau (produit) au localstorage
+        localStorage.setItem("panier", JSON.stringify(articles))
+    }
+    // créer nouvelle méthode dans panier sans parametre 
+    // elle va récuperer string du localstrorage
+    // la transforme en tableau 
+    // fonction va retourner la totalité du panier 
+    cart(){
+        let recupArticles = localStorage.getItem("panier")
+        let tousLesArticles = JSON.parse(recupArticles)
+        return tousLesArticles;
+
+    }
 }
